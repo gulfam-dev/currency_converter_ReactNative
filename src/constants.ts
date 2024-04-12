f8cd035570d4fc3eb02a1a6240aa3fc8d99a2284
Gulfam <gulfam.dev@gmail.com>
2024-04-12 21:33:37 +0500

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,57 @@
+export const currencyByRupee: Currency[] = [
+    {
+      name: 'DOLLAR',
+      value: 0.0036,
+      flag: '🇺🇸',
+      symbol: '$',
+    },
+    {
+      name: 'EURO',
+      value: 0.0034,
+      flag: '🇪🇺',
+      symbol: '€',
+    },
+    {
+      name: 'POUND',
+      value: 0.0029,
+      flag: '🇬🇧',
+      symbol: '£',
+    },
+    {
+      name: 'RUBEL',
+      value: 0.33,
+      flag: '🇷🇺',
+      symbol: '₽',
+    },
+    {
+      name: 'AUS DOLLAR',
+      value: 0.0056,
+      flag: '🇦🇺',
+      symbol: 'A$',
+    },
+    {
+      name: 'CAN DOLLAR',
+      value: 0.0050,
+      flag: '🇨🇦',
+      symbol: 'C$',
+    },
+    {
+      name: 'YEN',
+      value: 0.55,
+      flag: '🇯🇵',
+      symbol: '¥',
+    },
+    {
+      name: 'DINAR',
+      value: 0.40,
+      flag: '🇰🇼',
+      symbol: 'KD',
+    },
+    {
+      name: 'BITCOIN',
+      value: 0.00,
+      flag: '🎰',
+      symbol: '₿',
+    },
+  ];
+  
